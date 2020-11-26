988c23383fd2d777cc6ce0e371ad16e1bbd4efd8
Ben Jones <benwjones76@outlook.com>
2020-11-26 14:35:52 +0000

    started testing a navbar component

---
+++ fakebook/src/NavBar.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <header className="NavBar-header">
+            <p>Navigation goes here</p>
+            </header>
+        </div>
+    );
+}
+
+export default NavBar;
