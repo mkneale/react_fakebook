2a05fb7945ab2ee7c6e3603a24827a7580983f43
Melissa <mkneale1@hotmail.com>
2020-11-26 15:31:32 +0000

    Adding bootstrap components into NavBar

--- fakebook/src/NavBar.js
+++ fakebook/src/NavBar.js
@@ -1,13 +1,22 @@
 import './style.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function NavBar() {
+function NavBarComponent() {
     return (
         <div className="NavBar">
-            <header className="NavBar-header">
-            <p>Navigation goes here</p>
-            </header>
+                <Navbar bg="light" expand="lg" fixed="top">
+                    <Navbar.Brand href="#home">FakeBook</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                        <Nav.Link href="#home">Sign In</Nav.Link>
+                        <Nav.Link href="#link">Sign Up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
         </div>
-    );
+);
 }
 
-export default NavBar;
+export default NavBarComponent;
