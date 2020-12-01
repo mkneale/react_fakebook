b0d865a16eb90395b4c772f51c49360f19fb9216
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-12-01 07:27:13 +0000

    before updateing

--- fakebook/src/App.js
+++ fakebook/src/App.js
@@ -1,17 +1,10 @@
 import './style.css';
-import React, {useEffect, useState, localStorage} from 'react';
+import React from 'react';
 
  export default class App extends React.Component {
-   useEffect(() => {
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser) {
-      const foundUser = JSON.parse(loggedInUser);
-      setUser(foundUser);
-    }
-  }, []);
    render() { return (
         <p>
-        You need to logged in to do anything here!
+        Hello world!
         </p>
     );
 }}
