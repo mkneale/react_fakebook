f408cc2d157e4223a739410a9e8c5eed77e9223a
Ben Jones <benwjones76@outlook.com>
2020-12-03 21:08:36 +0000

    Now we can like posts with a button

--- fakebook/src/components/Likes.js
+++ fakebook/src/components/Likes.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
 
 class Like extends Component {
 
+    constructor(props) {
+        super(props);
+        this.likePost = this.likePost.bind(this); 
+    };
+
+    likePost(postId, error) {
+        
+        console.log('The link was clicked from post id:', postId);
+        console.log('This is the user_Id', this.props.user_id);
+        console.log('This is the username', this.props.user_name);
+        fetch(`http://localhost:3080/posts/${postId}/like`, {
+        mode: 'cors',
+        method: 'POST',
+        body: {authorId: this.props.user_id, author: this.props.user_name}
+    })
+    window.location.reload();
+    };
+    
+
     render () {
+       
         return (
             <div> 
                <p> <img src={process.env.PUBLIC_URL + 'thumbs_up_icon.png'} width='25px' height='25px' />
-                 Likes: {this.props.likes}</p> 
-                 Post ID: {this.props.post_id}   
-                <p> User ID: {this.props.user_id} </p>
-                <p> User name: {this.props.user_name} </p>
+                 Likes: {this.props.likes}</p>  
+                <Button key={this.props.post_id} variant="primary" size="sm" onClick={() => { this.likePost(this.props.post_id) }}>
+                    Like
+                </Button>
             </div>
         )
         }
