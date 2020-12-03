294d70af5e1a42a5f4dba91981b0b71e264231fe
Ben Jones <benwjones76@outlook.com>
2020-12-03 14:39:42 +0000

    Can get single post with route

--- fakebook/src/components/SinglePost.js
+++ fakebook/src/components/SinglePost.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class SinglePost extends Component {
+
+    componentDidMount() {
+        const { match: { params } } = this.props;
+
+        this.setState({user: window.localStorage.getItem('user')})
+
+          fetch(`http://localhost:3080/posts/${params.postId}`, {mode: 'cors', method: 'GET'})
+          .then(res => res.json())
+          .then(
+            (result) => {
+              console.log(result.post);
+              return result.post;
+            },
+            (error) => {
+                throw error;
+            }
+          )
+    }
+
     render () {
         return (
             <div>
