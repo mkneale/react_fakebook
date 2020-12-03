d090487278fa2f8e7f4b64d85945b9e244e153c3
Ben Jones <benwjones76@outlook.com>
2020-12-03 14:44:43 +0000

    cleaned up comment ready for actual stuff

--- fakebook/src/components/Comment.js
+++ fakebook/src/components/Comment.js
@@ -4,11 +4,11 @@ class Comment extends Component {
     render () {
         return (
             <div>
-                <Greeting greeting={{ text: 'Welcome to React' }} />
+                
             </div>
         )
         }
 }
-const Greeting = ({ greeting }) => <h1>{greeting.text}</h1>;
+
 
 export default Comment;
