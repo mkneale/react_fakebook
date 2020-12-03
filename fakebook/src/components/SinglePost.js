141b9f449b77adff4f743c5a118772bdfacccb29
Ben Jones <benwjones76@outlook.com>
2020-12-03 14:45:50 +0000

    hadnt saved

--- fakebook/src/components/SinglePost.js
+++ fakebook/src/components/SinglePost.js
@@ -23,11 +23,10 @@ class SinglePost extends Component {
     render () {
         return (
             <div>
-                <Greeting greeting={{ text: 'Welcome to React' }} />
+                
             </div>
         )
         }
 }
-const Greeting = ({ greeting }) => <h1>{greeting.text}</h1>;
 
 export default SinglePost;
