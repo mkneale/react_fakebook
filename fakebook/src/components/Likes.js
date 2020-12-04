845ad0773fdb526decd78a2aa5b718f07f0a8edd
Melissa <mkneale1@hotmail.com>
2020-12-04 12:46:21 +0000

    Unlike button added

--- fakebook/src/components/Likes.js
+++ fakebook/src/components/Likes.js
@@ -23,16 +23,33 @@ class Like extends Component {
     window.location.reload();
     };
 
+    unlikePost(postId, error) {
+
+        console.log('The link was clicked from post id:', postId);
+        console.log('This is the user_Id', this.props.user_id);
+        console.log('This is the username', this.props.user_name);
+        fetch(`http://localhost:3080/posts/${postId}/unlike`, {
+        mode: 'cors',
+        method: 'POST',
+    })
+    window.location.reload();
+    };
+
+
 
     render () {
 
         return (
             <div>
-               <p>
-                 Likes: {this.props.likes} &nbsp;&nbsp;
-                <Button key={this.props.post_id} variant="primary" size="sm" onClick={() => { this.likePost(this.props.post_id) }}>
-                  <img src={process.env.PUBLIC_URL + 'thumbs_up_icon.png'} width='25px' height='25px' alt="thumbs-up-image"/>  Like
-                </Button></p>
+                <p>
+                    Likes: {this.props.likes} &nbsp;&nbsp;
+                    <Button key={this.props.post_id} variant="primary" size="sm" onClick={() => { this.likePost(this.props.post_id) }}>
+                    <img src={process.env.PUBLIC_URL + 'thumbs_up_icon.png'} width='25px' height='25px' alt="thumbs-up-icon"/>  Like
+                    </Button> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <Button key={this.props.post_id} variant="primary" size="sm" onClick={() => { this.unlikePost(this.props.post_id) }}>
+                        <img src={process.env.PUBLIC_URL + 'thumbs_down_icon.png'} width='25px' height='25px' alt="thumbs-down-icon"/>  Unlike
+                    </Button>
+                </p>
             </div>
         )
         }
